f8fd436f558ec24eccae83380a1d71ec0b32ba7c
mohammedkh52 <khm61574@gmail.com>
2026-07-04 18:06:50 +0100

    Create ChatViewModel and chat input

--- app/src/main/java/com/example/nexaai1/MainActivity.kt
+++ app/src/main/java/com/example/nexaai1/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.activity.compose.setContent
 import com.example.nexaai1.ui.screens.ChatScreen
 import com.example.nexaai1.ui.theme.NEXAAI1Theme
 
@@ -20,11 +19,9 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NEXAAI1Theme {
-
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-
                     ChatScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
--- app/src/main/java/ui/screens/chatScreen.kt
+++ app/src/main/java/ui/screens/chatScreen.kt
@@ -2,17 +2,22 @@ package com.example.nexaai1.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.nexaai1.ui.theme.NEXAAI1Theme
+import com.example.nexaai1.viewmodel.ChatViewModel
 
 @Composable
 fun ChatScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    chatViewModel: ChatViewModel = viewModel()
 ) {
+
+    var text by remember { mutableStateOf("") }
 
     Column(
         modifier = modifier
@@ -30,7 +35,6 @@ fun ChatScreen(
         Card(
             modifier = Modifier.fillMaxWidth()
         ) {
-
             Text(
                 text = "مرحبًا محمد 👋\nكيف أستطيع مساعدتك اليوم؟",
                 modifier = Modifier.padding(16.dp)
@@ -40,8 +44,8 @@ fun ChatScreen(
         Spacer(modifier = Modifier.weight(1f))
 
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = text,
+            onValueChange = { text = it },
             modifier = Modifier.fillMaxWidth(),
             placeholder = {
                 Text("اكتب رسالتك...")
@@ -51,7 +55,10 @@ fun ChatScreen(
         Spacer(modifier = Modifier.height(10.dp))
 
         Button(
-            onClick = {},
+            onClick = {
+                chatViewModel.sendMessage(text)
+                text = ""
+            },
             modifier = Modifier.align(Alignment.End)
         ) {
             Text("إرسال")
@@ -62,7 +69,6 @@ fun ChatScreen(
 @Preview(showBackground = true)
 @Composable
 fun ChatScreenPreview() {
-
     NEXAAI1Theme {
         ChatScreen()
     }
